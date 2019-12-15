0b53b064750c1a7719864d9cf47bd0afd77bff05
Rahul Ranjan <rahulranjanmca@gmail.com>
2019-12-15 18:32:38 +0530

    fix

---
+++ build/precache-manifest.1809be0bb78fb5e93e5b03b3e708073b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "119e9fa82664cf95afe4233e710d77d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a48e0b4a86b050702e5",
+    "url": "/static/css/2.deb05650.chunk.css"
+  },
+  {
+    "revision": "d9c501b0fe36b37d3d99",
+    "url": "/static/css/main.dabeb7c6.chunk.css"
+  },
+  {
+    "revision": "1a48e0b4a86b050702e5",
+    "url": "/static/js/2.7b9e1658.chunk.js"
+  },
+  {
+    "revision": "d9c501b0fe36b37d3d99",
+    "url": "/static/js/main.0d67412a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
